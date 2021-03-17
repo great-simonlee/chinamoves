d00e70605b3169e3a938e61a76a9cc470b845121
Simon Lee <great.simonlee@gmail.com>
2021-03-17 12:19:11 -0400

    Mainpage card has been created

--- src/components/main_lastest_boston_listings/main_lastest_boston_listings.jsx
+++ src/components/main_lastest_boston_listings/main_lastest_boston_listings.jsx
@@ -2,9 +2,73 @@ import React from 'react';
 import styles from './main_lastest_boston_listings.module.css';
 
 const MainLastestBostonListings = (props) => (
-    <div className={ styles.listingcontainer }>
-        <h1>最新房源：波士顿</h1>
-    </div>
+    <>
+        <div className={ styles.title }>
+            <h1>最新房源：波士顿</h1>
+        </div>
+        <div className={ styles.listingContainer }>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
     );
 
 export default MainLastestBostonListings;
--- src/components/main_lastest_nyc_listings/main_lastest_nyc_listings.jsx
+++ src/components/main_lastest_nyc_listings/main_lastest_nyc_listings.jsx
@@ -4,14 +4,64 @@ import styles from './main_lastest_nyc_listings.module.css';
 const MainLastestNycListings = (props) => (
     <>
         <div className={ styles.title }>
-            <h1>最新房源：纽约-曼哈顿 Hello world</h1>
+            <h1>最新房源：纽约-曼哈顿</h1>
         </div>
         <div className={ styles.listingContainer }>
             <div className={ styles.listingCard }>
-                <img src="/chinamoves/images/listing_item.jpeg"/>
-                <div>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
                     <span>606 57th St</span>
-                    <div>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
+                        <span>Studio</span>
+                        <span>$2,500 / Month</span>
+                    </div>
+                </div>
+            </div>
+            <div className={ styles.listingCard }>
+                <img className={ styles.listingImg } src="/chinamoves/images/listing_item.jpeg"/>
+                <div className={ styles.listingInfo }>
+                    <span>606 57th St</span>
+                    <div className={ styles.typePrice }>
                         <span>Studio</span>
                         <span>$2,500 / Month</span>
                     </div>
@@ -22,3 +72,5 @@ const MainLastestNycListings = (props) => (
     );
 
 export default MainLastestNycListings;
+
+// Listing card should contain location, type, price, and google maps link
