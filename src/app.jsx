57fd9b4026a167f0126419d87d1d73014e009779
Simon Lee <great.simonlee@gmail.com>
2021-03-17 15:40:42 -0400

    The page order has been changed

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import './app.css';
 import BodyContent from './components/body_container/body_container';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import AboutUs from './components/aboutus/aboutus';
 import AboutAgents from './components/aboutagents/about_agents';
 
@@ -10,12 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact>
-          <Header />
-          {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/Tf_t5d63pHA?autoplay=1&controls=0&mute=1&disablekb=1&playlist=Tf_t5d63pHA&loop=1" frameBorder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
-          <BodyContent />
-          <Footer />
-        </Route>
+
         <Route path="/aboutus">
           <Header />
           <AboutUs />
@@ -27,6 +22,12 @@ function App() {
           <Footer />
         </Route>
       </Switch>
+      <Route path={["/", ""]}>
+          <Header />
+          {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/Tf_t5d63pHA?autoplay=1&controls=0&mute=1&disablekb=1&playlist=Tf_t5d63pHA&loop=1" frameBorder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+          <BodyContent />
+          <Footer />
+        </Route>
     </BrowserRouter>
   );
 }
