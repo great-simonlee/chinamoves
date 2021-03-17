46595ec1b8154216892c5415e570998fbc6cf424
Simon Lee <great.simonlee@gmail.com>
2021-03-17 10:46:53 -0400

    deployment test

--- src/components/main_lastest_nyc_listings/main_lastest_nyc_listings.jsx
+++ src/components/main_lastest_nyc_listings/main_lastest_nyc_listings.jsx
@@ -4,18 +4,17 @@ import styles from './main_lastest_nyc_listings.module.css';
 const MainLastestNycListings = (props) => (
     <>
         <div className={ styles.title }>
-            <h1>最新房源：纽约-曼哈顿</h1>
+            <h1>最新房源：纽约-曼哈顿 Hello world</h1>
         </div>
         <div className={ styles.listingContainer }>
             <div className={ styles.listingCard }>
-                <img src="/images/listing_item.jpeg"/>
+                <img src={ process.env.PUBLIC_URL + "/images/listing_item.jpeg" }/>
                 <div>
                     <span>606 57th St</span>
                     <div>
                         <span>Studio</span>
                         <span>$2,500 / Month</span>
                     </div>
-                    
                 </div>
             </div>
         </div>
