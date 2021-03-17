97251ccae3f1926e87f5f058e9d4324410889301
Simon Lee <great.simonlee@gmail.com>
2021-03-16 20:41:04 -0400

    The address of the images has been updated

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ import styles from './header.module.css';
 const Header = (props) => (
         <header className={ styles.header }>
             <div className={ styles.logo }>
-                <img className={ styles.logoImg } src="/images/logo.png" alt="ChinaMoves"/>
+                <img className={ styles.logoImg } src={ process.env.PUBLIC_URL + "/images/logo.png" } alt="ChinaMoves"/>
                 <p className={ styles.logoText }>CHINA</p><p className={ styles.logoTextMid }>MOVES</p><p className={ styles.logoText }>USA</p><p className={ styles.logoTextEnd }>.com</p>
             </div>
             <div className={ styles.headerBtn }>
--- src/components/main_image/main_image.jsx
+++ src/components/main_image/main_image.jsx
@@ -3,7 +3,7 @@ import styles from './main_image.module.css';
 
 const MainImage = (props) => (
             <div className={ styles.mainImgContainer }>
-                <img className={ styles.mainImg } src="/images/main_image.jpg" alt="main_image"/>
+                <img className={ styles.mainImg } src={ process.env.PUBLIC_URL + "/images/main_image.jpg" } alt="main_image"/>
                 <p className={ styles.mainImgText }>MAKE YOUR BEST MOVE</p>
             </div>
     );
